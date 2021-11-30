b55da7e97aafe25ff0d6bdb6faeb30a103ab1600
shaofengzeng <shaofengzeng@163.com>
2021-11-30 15:00:04 +0800

    add default config

--- dataset/synthetic_shapes.py
+++ dataset/synthetic_shapes.py
@@ -12,6 +12,57 @@ from dataset.utils.photometric_augmentation import PhotoAugmentor
 
 
 class SyntheticShapes(Dataset):
+    default_config = {
+        'primitives': 'all',
+        'truncate': {},
+        'validation_size': -1,
+        'test_size': -1,
+        'on-the-fly': False,
+        'cache_in_memory': False,
+        'suffix': None,
+        'add_augmentation_to_test_set': False,
+        'num_parallel_calls': 10,
+        'generation': {
+            'split_sizes': {'training': 10000, 'validation': 200, 'test': 500},
+            'image_size': [960, 1280],
+            'random_seed': 0,
+            'params': {
+                'generate_background': {
+                    'min_kernel_size': 150, 'max_kernel_size': 500,
+                    'min_rad_ratio': 0.02, 'max_rad_ratio': 0.031},
+                'draw_stripes': {'transform_params': (0.1, 0.1)},
+                'draw_multiple_polygons': {'kernel_boundaries': (50, 100)}
+            },
+        },
+        'preprocessing': {
+            'resize': [240, 320],
+            'blur_size': 11,
+        },
+        'augmentation': {
+            'photometric': {
+                'enable': False,
+                'primitives': 'all',
+                'params': {},
+                'random_order': True,
+            },
+            'homographic': {
+                'enable': False,
+                'params': {},
+                'valid_border_margin': 0,
+            },
+        }
+    }
+    drawing_primitives = [
+        'draw_lines',
+        'draw_polygon',
+        'draw_multiple_polygons',
+        'draw_ellipses',
+        'draw_star',
+        'draw_checkerboard',
+        'draw_stripes',
+        'draw_cube',
+        'gaussian_noise'
+    ]
 
     def __init__(self, config, task, device='cpu'):
         """
@@ -20,14 +71,12 @@ class SyntheticShapes(Dataset):
             task: 'train','validation','test' or their combinations
             transforms: transformation for samples
         """
-        # Update config
         self.device = device
-        self.config = config #dict_update(getattr(self, 'default_config', {}), config)
+        self.config = dict_update(getattr(self, 'default_config', {}), config)
         self.task = task if isinstance(task, (list, tuple)) else [task, ]
         self.photo_aug = PhotoAugmentor(config['augmentation']['photometric'])
         #load data, if no data generate some
         self.samples = self._init_dataset()
-
 
     def dump_primitive_data(self, primitive):
         output_dir = Path(self.config['data_dir'], primitive)
