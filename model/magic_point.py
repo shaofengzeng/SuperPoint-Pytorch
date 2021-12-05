917ba2e081e7b0b5bb7de69f264565bedc7eaf41
shaofengzeng <shaofengzeng@163.com>
2021-12-05 20:50:41 +0800

    add using bn

--- model/magic_point.py
+++ model/magic_point.py
@@ -1,7 +1,7 @@
 #-*-coding:utf8-*-
 import torch
 from solver.nms import box_nms
-from model.modules.cnn.vgg_backbone import VGGBackboneBN
+from model.modules.cnn.vgg_backbone import VGGBackboneBN,VGGBackbone
 from model.modules.cnn.cnn_heads import DetectorHead
 
 
@@ -9,14 +9,17 @@ from model.modules.cnn.cnn_heads import DetectorHead
 class MagicPoint(torch.nn.Module):
     """ Pytorch definition of SuperPoint Network. """
 
-    def __init__(self, config, input_channel=1, grid_size=8, device='cpu'):
+    def __init__(self, config, input_channel=1, grid_size=8, using_bn=True, device='cpu'):
         super(MagicPoint, self).__init__()
         self.nms = config['nms']
         self.det_thresh = config['det_thresh']
         self.topk = config['topk']
+        if using_bn:
+            self.backbone = VGGBackboneBN(config['backbone']['vgg'], input_channel, device=device)
+        else:
+            self.backbone = VGGBackbone(config['backbone']['vgg'], input_channel, device=device)
 
-        self.backbone = VGGBackboneBN(config['backbone']['vgg'], input_channel, device=device)
-        self.detector_head = DetectorHead(input_channel=128, grid_size=grid_size,using_bn=True)
+        self.detector_head = DetectorHead(input_channel=128, grid_size=grid_size,using_bn=using_bn)
 
     def forward(self, x):
         """ Forward pass that jointly computes unprocessed point and descriptor
@@ -25,7 +28,6 @@ class MagicPoint(torch.nn.Module):
           x: Image pytorch tensor shaped N x 1 x H x W.
         Output
           semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
-          desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
         """
         if isinstance(x, dict):
             feat_map = self.backbone(x['img'])
