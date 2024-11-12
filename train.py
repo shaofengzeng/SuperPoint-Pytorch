b8664fc5023de61cbac0188e6fc83699bfa00332
Shao-Feng Zeng <shaofengzeng@163.com>
2024-11-12 11:18:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -44,70 +44,61 @@ model_dict_map= \
 def train_eval(model, dataloader, config):
     optimizer = torch.optim.Adam(model.parameters(), lr=config['solver']['base_lr'])
 
-    try:
-        # start training
-        for epoch in range(config['solver']['epoch']):
-            model.train()
-            mean_loss = []
-            for i, data in tqdm(enumerate(dataloader['train'])):
-                prob, desc, prob_warp, desc_warp = None, None, None, None
-                if config['model']['name']=='magicpoint' and config['data']['name']=='coco':
-                    data['raw'] = data['warp']
-                    data['warp'] = None
+    # start training
+    for epoch in range(config['solver']['epoch']):
+        model.train()
+        mean_loss = []
+        for i, data in tqdm(enumerate(dataloader['train'])):
+            prob, desc, prob_warp, desc_warp = None, None, None, None
+            if config['model']['name']=='magicpoint' and config['data']['name']=='coco':
+                data['raw'] = data['warp']
+                data['warp'] = None
 
-                raw_outputs = model(data['raw'])
+            raw_outputs = model(data['raw'])
 
-                ## for superpoint
-                if config['model']['name']!='magicpoint':#train superpoint
-                    warp_outputs = model(data['warp'])
-                    prob, desc, prob_warp, desc_warp = raw_outputs['det_info'], \
-                                                       raw_outputs['desc_info'], \
-                                                       warp_outputs['det_info'],\
-                                                       warp_outputs['desc_info']
-                else:
-                    prob = raw_outputs #train magicpoint
+            ## train superpoint
+            if config['model']['name']!='magicpoint':
+                warp_outputs = model(data['warp'])
+                prob, desc, prob_warp, desc_warp = raw_outputs['det_info'], \
+                                                   raw_outputs['desc_info'], \
+                                                   warp_outputs['det_info'],\
+                                                   warp_outputs['desc_info']
+            else:#train magicpoint
+                prob = raw_outputs
 
-                ##loss
-                loss = loss_func(config['solver'], data, prob, desc,
-                                 prob_warp, desc_warp, device)
+            ##loss
+            loss = loss_func(config['solver'], data, prob, desc,
+                             prob_warp, desc_warp, device, "TRAIN")
 
-                mean_loss.append(loss.item())
-                #reset
-                model.zero_grad()
-                loss.backward()
-                optimizer.step()
+            mean_loss.append(loss.item())
+            #reset
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-                if (i%500==0):
-                    print('Epoch [{}/{}], Step [{}/{}], LR [{}], Loss: {:.3f}'
-                          .format(epoch, config['solver']['epoch'], i, len(dataloader['train']),
-                                  optimizer.state_dict()['param_groups'][0]['lr'], np.mean(mean_loss)))
-                    mean_loss = []
+            if (i%100==0):
+                print('Train Epoch [{}/{}], Step [{}/{}], LR [{}], Loss: {:.3f}'
+                      .format(epoch, config['solver']['epoch'], i, len(dataloader['train']),
+                              optimizer.state_dict()['param_groups'][0]['lr'], np.mean(mean_loss)))
+                mean_loss = []
+        #end one epoch
 
-                ##do evaluation
-                save_iter = int(0.5*len(dataloader['train']))#half epoch
-                if (i%save_iter==0 and i!=0) or (i+1)==len(dataloader['train']):
-                    model.eval()
-                    eval_loss = do_eval(model, dataloader['test'], config, device)
-                    model.train()
+        #evaluation
+        model.eval()
+        eval_loss = do_eval(model, dataloader['test'], config, device)
+        save_path = os.path.join(config['solver']['save_dir'],
+                                 config['solver']['model_name'] + '_{}_{}.pth').format(epoch, round(eval_loss, 3))
+        torch.save(model.state_dict(), save_path)
+        print('Eval Epoch [{}/{}], Step [{}/{}], Eval loss {:.3f}, Checkpoint saved to {}'
+              .format(epoch, config['solver']['epoch'], i, len(dataloader['train']), eval_loss, save_path))
 
-                    save_path = os.path.join(config['solver']['save_dir'],
-                                             config['solver']['model_name'] + '_{}_{}.pth').format(epoch, round(eval_loss, 3))
-                    torch.save(model.state_dict(), save_path)
-                    print('Epoch [{}/{}], Step [{}/{}], Eval loss {:.3f}, Checkpoint saved to {}'
-                          .format(epoch, config['solver']['epoch'], i, len(dataloader['train']), eval_loss, save_path))
-                    mean_loss = []
-
-    except KeyboardInterrupt:
-        torch.save(model.state_dict(), "./export/key_interrupt_model.pth")
 
 @torch.no_grad()
 def do_eval(model, dataloader, config, device):
-    mean_loss = []
-    truncate_n = max(int(0.1 * len(dataloader)), 100)  # 0.1 of test dataset for eval
+    _eval_loss = []
 
     for ind, data in tqdm(enumerate(dataloader)):
-        if ind>truncate_n:
-            break
+
         prob, desc, prob_warp, desc_warp = None, None, None, None
         if config['model']['name'] == 'magicpoint' and config['data']['name'] == 'coco':
             data['raw'] = data['warp']
@@ -126,18 +117,17 @@ def do_eval(model, dataloader, config, device):
 
         # compute loss
         loss = loss_func(config['solver'], data, prob, desc,
-                         prob_warp, desc_warp, device)
+                         prob_warp, desc_warp, device, "EVAL")
 
-        mean_loss.append(loss.item())
-    mean_loss = np.mean(mean_loss)
+        _eval_loss.append(loss.item())
 
-    return mean_loss
+    res = np.mean(_eval_loss)
+
+    return res
 
 
 
 if __name__=='__main__':
-
-    torch.multiprocessing.set_start_method('spawn')
 
     parser = argparse.ArgumentParser()
     parser.add_argument("config")
@@ -146,14 +136,14 @@ if __name__=='__main__':
 
     config_file = args.config
     assert (os.path.exists(config_file))
-    ##
+
     with open(config_file, 'r') as fin:
         config = yaml.safe_load(fin)
 
     if not os.path.exists(config['solver']['save_dir']):
         os.makedirs(config['solver']['save_dir'])
 
-    device = 'cuda:2' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
     ##Make Dataloader
     data_loaders = None
@@ -171,7 +161,7 @@ if __name__=='__main__':
                                             collate_fn=datasets['train'].batch_collator),
                         'test': DataLoader(datasets['test'], batch_size=config['solver']['test_batch_size'], shuffle=True,
                                            collate_fn=datasets['test'].batch_collator)}
-    ##Make model
+    ## Make model
     if config['model']['name'] == 'superpoint':
         model = SuperPointBNNet(config['model'], device=device, using_bn=config['model']['using_bn'])
     elif config['model']['name'] == 'magicpoint':
